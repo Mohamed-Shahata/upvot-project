4ce2e2ed6e9b0113c83856080fc264e18bf81cb0
mohamed <mohamedmrslan@gmail.com>
2024-12-05 13:56:29 +0200

    update file user.validationSchema.js

--- src/modules/user.validationSchemas.js
+++ src/modules/user.validationSchemas.js
@@ -1,6 +1,15 @@
 
 import Joi from "joi";
 
+
+const ageRole = (value, heleprs) => {
+    if (value <= 18)
+        return heleprs.message("age must be genarter than 18")
+    // return heleprs.error("any.invalid")
+    // throw new Error("age must be genarter than 18")
+    return value
+}
+
 export const signUpSchema = {
     body: Joi.object({
         username: Joi.string().min(5).max(10).alphanum().required().messages({
@@ -9,8 +18,9 @@ export const signUpSchema = {
         email: Joi.string().email({ tlds: { allow: ["com", "org", "yahoo"] }, minDomainSegments: 1 }),
         password: Joi.string(),
         cpass: Joi.string().valid(Joi.ref("password")),
-        age: Joi.number().min(15).max(100),
+        age: Joi.number().custom(ageRole),
         gender: Joi.string().valid("male", "female")
-    })//.options({ presence: "required" })
+    })
+        // .options({ presence: "required" })
         .with("password", "cpass").with("email", "password")
 }
