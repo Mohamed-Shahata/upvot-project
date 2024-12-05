b580124e7bb5a592d57a6602e14bb9032884af9a
mohamed <mohamedmrslan@gmail.com>
2024-12-05 13:13:29 +0200

    update

--- src/modules/user.validationSchemas.js
+++ src/modules/user.validationSchemas.js
@@ -3,7 +3,9 @@ import Joi from "joi";
 
 export const signUpSchema = {
     body: Joi.object({
-        username: Joi.string().min(5).max(10).alphanum().required().messages({ 'any.required': 'please enter your username' }),
+        username: Joi.string().min(5).max(10).alphanum().required().messages({
+            'any.required': 'please enter your username'
+        }),
         email: Joi.string().email({ tlds: { allow: ["com", "org", "yahoo"] }, minDomainSegments: 1 }),
         password: Joi.string(),
         cpass: Joi.string().valid(Joi.ref("password")),
